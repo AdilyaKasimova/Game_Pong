3e672693b360bde5df07473e0e8a665183a6fc5e
AdilyaKasimova <adalkasimova@edu.hse.ru>
2024-05-14 00:06:13 +0300

    Created a resizable window and a buffer to assign colors to pixels

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -4,17 +4,42 @@
 
 bool running = true;
 
+void* buffer_memory;
+int buffer_width;
+int buffer_height;
+BITMAPINFO buffer_bitmap_info;
+
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) { // Это окно обратного вызова
 	LRESULT result = 0;
 	switch (uMsg) {
-	case WM_CLOSE:
-	case WM_DESTROY: { // Закрытие игры
-		running = false;
-	} break;
+		case WM_CLOSE:
+		case WM_DESTROY: { // Закрытие игры
+			running = false;
+		} break;
 
-	default: {
-		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
-	}
+		case WM_SIZE: { // Здесь происходит учет размера окна
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			buffer_width = rect.right - rect.left;
+			buffer_height = rect.bottom - rect.top;
+
+			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+
+			if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
+			buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			
+			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
+			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+			buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
+			buffer_bitmap_info.bmiHeader.biPlanes = 1;
+			buffer_bitmap_info.bmiHeader.biBitCount = 32;
+			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
 	}
 	return result;
 }
@@ -31,6 +56,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	// Create Window
 	HWND window = CreateWindow(window_class.lpszClassName, TEXT("Pong"), WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0); //CreateWindow(lpClassName, lpWindowName, dwStyle, x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+	HDC hdc = GetDC(window);
 
 	while (running) {
 		// Input
@@ -40,7 +66,16 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 			DispatchMessage(&message);
 		}
 		// Simulate
+		unsigned int* pixel = (unsigned int*) buffer_memory;
+		for (int y = 0; y < buffer_height; y++) {
+			for (int x = 0; x < buffer_width; x++) {
+				*pixel++ = 0xfb8500;
+			}
+		}
 
 		// Render
+		StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+
+
 	}
 }
