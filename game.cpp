cc0042f310e1ae83a0c6a97c9a2fc04af9765bba
AdilyaKasimova <adalkasimova@edu.hse.ru>
2024-06-04 13:19:57 +0300

    Formatted the code

--- game.cpp
+++ game.cpp
@@ -7,6 +7,31 @@ float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y, ball_half_size = 1;
 
+internal void
+simulate_player(float *p, float *dp, float ddp, float dt) {
+	ddp -= *dp * 10.f; // Сила трения
+	*p = *p + *dp * dt + ddp * dt * dt * .5f; // Уравнения для равноускоренного движения
+	*dp = *dp + ddp * dt;
+
+	if (*p + player_half_size_y > arena_half_size_y) { // Столкновение игрока с верхней границей арены 
+		*p = arena_half_size_y - player_half_size_y;
+		*dp = 0;
+	}
+	else if (*p - player_half_size_y < -arena_half_size_y) { // Столкновение игрока с нижней границей арены 
+		*p = -arena_half_size_y + player_half_size_y;
+		*dp = 0;
+	}
+}
+
+internal bool
+aabb_vs_aabb(float ball_p_x, float ball_p_y, float ball_hs, 
+	float player_p_x, float player_p_y, float player_hs_x, float player_hs_y) { // Проверяет факт столкновения мяча с игроком
+	return (ball_p_x + ball_hs > player_p_x - player_hs_x &&
+		ball_p_x - ball_hs < player_p_x + player_hs_x &&
+		ball_p_y + ball_hs > player_p_y - player_hs_y &&
+		ball_p_y + ball_hs < player_p_y + player_hs_y);
+}
+
 internal void 
 simulate_game(Input* input, float dt){
 	clear_screen(0xff5500);
@@ -20,76 +45,53 @@ simulate_game(Input* input, float dt){
 	if (is_down(BUTTON_W)) player_2_ddp += 1500;
 	if (is_down(BUTTON_S)) player_2_ddp -= 1500;
 
-	player_1_ddp -= player_1_dp * 10.f; // Сила трения
-	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f; // Уравнения для равноускоренного движения
-	player_1_dp += player_1_ddp * dt;
+	simulate_player(&player_1_p, &player_1_dp, player_1_ddp, dt);
+	simulate_player(&player_2_p, &player_2_dp, player_2_ddp, dt);
 
-	if (player_1_p + player_half_size_y > arena_half_size_y) { // Столкновение правого игрока с верхней границей арены 
-		player_1_p = arena_half_size_y - player_half_size_y;
-		player_1_dp = 0;
+
+	// Simulate Ball
+	{
+		ball_p_x += ball_dp_x * dt;
+		ball_p_y += ball_dp_y * dt;
+
+
+		if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, 80, player_1_p, player_half_size_x, player_half_size_y)) { // Столкновение мяча с правым игроком
+			ball_p_x = 80 - player_half_size_x - ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * .75f;
+		}
+		else if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, -80, player_2_p, player_half_size_x, player_half_size_y)) { // Столкновение мяча с левым игроком
+			ball_p_x = -80 + player_half_size_x + ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_2_p) * 2 + player_2_dp * .75f;
+		}
+
+		if (ball_p_y + ball_half_size > arena_half_size_y) { // Столкновение мяча с верхней границей арены
+			ball_p_y = arena_half_size_y - ball_half_size;
+			ball_dp_y *= -1;
+		}
+
+		if (ball_p_y - ball_half_size < -arena_half_size_y) { // Столкновение мяча с нижней границей арены
+			ball_p_y = -arena_half_size_y + ball_half_size;
+			ball_dp_y *= -1;
+		}
+
+		if (ball_p_x + ball_half_size > arena_half_size_x) { // Столкновение мяча с правой границей арены
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+		}
+		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+		}
 	}
-	else if (player_1_p - player_half_size_y < -arena_half_size_y) { // Столкновение правого игрока с нижней границей арены 
-		player_1_p = -arena_half_size_y + player_half_size_y;
-		player_1_dp = 0;
-	}
 
-	player_2_ddp -= player_2_dp * 10.f; 
-	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * .5f;
-	player_2_dp += player_2_ddp * dt;
-
-	if (player_2_p + player_half_size_y > arena_half_size_y) { // Столкновение левого игрока с верхней границей арены 
-		player_2_p = arena_half_size_y - player_half_size_y;
-		player_2_dp = 0;
-	}
-	else if (player_2_p - player_half_size_y < -arena_half_size_y) { // Столкновение левого игрока с нижней границей арены 
-		player_2_p = -arena_half_size_y + player_half_size_y;
-		player_2_dp = 0;
-	}
-
-	ball_p_x += ball_dp_x * dt;
-	ball_p_y += ball_dp_y * dt;
-
+	// Rendering
 	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff); // Мяч
-
-	if (ball_p_x + ball_half_size > 80 - player_half_size_x && 
-		ball_p_x - ball_half_size < 80 + player_half_size_x &&
-		ball_p_y + ball_half_size > player_1_p - player_half_size_y &&
-		ball_p_y + ball_half_size < player_1_p + player_half_size_y) { // Столкновение мяча с правым игроком
-		ball_p_x = 80 - player_half_size_x - ball_half_size;
-		ball_dp_x *= -1;
-		ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * .75f;
-	}
-	else if (ball_p_x + ball_half_size > -80 - player_half_size_x &&
-		ball_p_x - ball_half_size < -80 + player_half_size_x &&
-		ball_p_y + ball_half_size > player_2_p - player_half_size_y &&
-		ball_p_y + ball_half_size < player_2_p + player_half_size_y) { // Столкновение мяча с левым игроком
-		ball_p_x = -80 + player_half_size_x + ball_half_size;
-		ball_dp_x *= -1;
-		ball_dp_y = (ball_p_y - player_2_p) * 2 + player_2_dp * .75f;
-	}
-
-	if (ball_p_y + ball_half_size > arena_half_size_y) { // Столкновение мяча с верхней границей арены
-		ball_p_y = arena_half_size_y - ball_half_size;
-		ball_dp_y *= -1;
-	}
-
-	if (ball_p_y - ball_half_size < -arena_half_size_y) { // Столкновение мяча с нижней границей арены
-		ball_p_y = -arena_half_size_y + ball_half_size;
-		ball_dp_y *= -1;
-	}
-
-	if (ball_p_x + ball_half_size > arena_half_size_x) { // Столкновение мяча с правой границей арены
-		ball_dp_x *= -1;
-		ball_dp_y = 0;
-		ball_p_x = 0;
-		ball_p_y = 0;
-	}
-	else if (ball_p_x - ball_half_size < -arena_half_size_x) {
-		ball_dp_x *= -1;
-		ball_dp_y = 0;
-		ball_p_x = 0;
-		ball_p_y = 0;
-	}
 
 	draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000); // Игрок правый
 	draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000); // Игрок левый
