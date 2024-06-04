cb815b6d6b5311abe3c355e039a758fcfd59dc3b
AdilyaKasimova <adalkasimova@edu.hse.ru>
2024-06-04 16:38:00 +0300

    Created a score counter

--- game.cpp
+++ game.cpp
@@ -7,6 +7,8 @@ float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y, ball_half_size = 1;
 
+int player_1_score, player_2_score;
+
 internal void
 simulate_player(float *p, float *dp, float ddp, float dt) {
 	ddp -= *dp * 10.f; // —ила трени€
@@ -81,14 +83,20 @@ simulate_game(Input* input, float dt){
 			ball_dp_y = 0;
 			ball_p_x = 0;
 			ball_p_y = 0;
+			player_2_score++;
 		}
-		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+		else if (ball_p_x - ball_half_size < -arena_half_size_x) { // —толкновение м€ча с левой границей арены
 			ball_dp_x *= -1;
 			ball_dp_y = 0;
 			ball_p_x = 0;
 			ball_p_y = 0;
+			player_1_score++;
 		}
 	}
+
+	draw_number(player_2_score, -10, 40, 1.f, 0xbbffbb);
+	draw_number(player_1_score, 10, 40, 1.f, 0xbbffbb);
+
 
 	// Rendering
 	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff); // ћ€ч
